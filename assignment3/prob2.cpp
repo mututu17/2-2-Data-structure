4bb0f62537607ecae76bdf444e8b61dbfab32a6b
mututu17 <chil_s_ng@naver.com>
2024-10-08 17:09:08 +0900

    solve: 과제 2번
    연결리스트 공부가 많이 됐습니다.
    정리를 해야될 거 같아요.

--- assignment3/prob2.cpp
+++ assignment3/prob2.cpp
@@ -90,7 +90,7 @@ void remove_under_num(int num)
 			}
 			//q는 그대로, p는 p->next로 이동
 			Total_Count--;
-			p = p->next; 
+			p = p->next;
 		}
 		else
 		{
@@ -98,6 +98,46 @@ void remove_under_num(int num)
 			p = p->next;
 		}
 	}
+	return;
+}
+void sort_by_count()
+{
+	Node* p = head; //head부터 리스트를 하나씩 삭제하여 
+	Node* new_head = nullptr; //new head부터 시작하는 리스트에 내림차순이 되도록 붙임
+	while (p != nullptr)
+	{
+		Node* tmp = new Node; //기존 리스트의 정보를 새 리스트에 저장
+		tmp->word = p->word;
+		tmp->count = p->count;
+		if (new_head == nullptr) //첫 노드를 생성할 때 예외처리
+		{
+			tmp->next = nullptr;
+			new_head = tmp;
+		}
+		else //ordered_insert
+		{
+			Node* prev = nullptr;
+			Node* q = new_head;
+			while (q != nullptr && q->count >= tmp->count)
+			{
+				prev = q;
+				q = q->next;
+			}
+			if (prev == nullptr) // 이전 노드가 없으므로
+			{
+				tmp->next = new_head;  //tmp(new_head) -> q 가 되도록 삽입
+				new_head = tmp;
+			}
+			else //이전 노드가 존재하므로
+			{
+				tmp->next = prev->next; //중간 또는 마지막에 삽입
+				prev->next = tmp;
+			}
+		}
+		p = p->next;
+		head = head->next;
+	}
+	head = new_head; //정렬이 끝난 후 head가 새 리스트를 가리키게 함
 	return;
 }
 int main()
@@ -110,5 +150,7 @@ int main()
 	}
 	print_all();
 	remove_under_num(10);
+	print_all();
+	sort_by_count(); //리스트를 등장 빈도의 내림차순으로 정렬함
 	print_all();
 }
