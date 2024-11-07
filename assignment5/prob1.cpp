73b534ce1094a61a67cfb3b9f1316c7a47f749f3
mututu17 <chil_s_ng@naver.com>
2024-11-07 10:26:42 +0900

    solve: 과제1번

---
+++ assignment5/prob1.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+#define max 1000000
+int mystack[max];
+int top_pos = -1;
+bool full()
+{
+	return top_pos == max;
+}
+bool empty()
+{
+	return top_pos == -1;
+}
+void push(int c)
+{
+	if (full())
+		throw runtime_error("stack_full");
+	mystack[++top_pos] = c;
+}
+void pop()
+{
+	if (empty())
+		throw runtime_error("stack_empty");
+	top_pos--;
+}
+int top()
+{
+	if (empty())
+		throw runtime_error("stack_empty");
+	return mystack[top_pos];
+}
+int main()
+{
+	string str;
+	cin >> str;
+	int count = 0;
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str[i] == '(')
+		{
+			push(++count);
+			cout << count << " ";
+		}
+		else if (str[i] == ')')
+		{
+			cout << top() << " ";
+			pop();
+		}
+	}
+	return 0;
+}
